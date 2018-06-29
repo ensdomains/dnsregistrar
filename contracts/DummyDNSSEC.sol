ee88ee69f90e37421ee32c1a2334f50fbcbca773
Makoto <inouemak@googlemail.com>
2018-06-29 15:32:31 +0200

    Rename contracts to case sensitive

---
+++ contracts/DummyDNSSEC.sol
@@ -0,0 +1,27 @@
+pragma solidity ^0.4.17;
+
+contract DummyDNSSEC {
+    uint16 expectedType;
+    bytes expectedName;
+    uint32 inception;
+    uint64 inserted;
+    bytes20 hash;
+
+    function setData(uint16 _expectedType, bytes _expectedName, uint32 _inception, uint64 _inserted, bytes _proof) public {
+      expectedType = _expectedType;
+      expectedName = _expectedName;
+      inception = _inception;
+      inserted = _inserted;
+      if(_proof.length == 0) {
+          hash = bytes20(0);
+      } else {
+          hash = bytes20(keccak256(_proof));
+      }
+    }
+
+    function rrdata(uint16 dnstype, bytes name) public constant returns(uint32, uint64, bytes20) {
+        require(dnstype == expectedType);
+        require(keccak256(name) == keccak256(expectedName));
+        return (inception, inserted, hash);
+    }
+}
