b1ddc43904c056e28854bb66ac7a38b7e7f1350c
Makoto <inouemak@googlemail.com>
2018-07-31 09:39:23 +0100

    Bring back migration on non ropsten network (using xyz tld not test tld)

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,10 +1,12 @@
 // Include ENSImplementation so that it gets compiled
 var ENSRegistry = artifacts.require("@ensdomains/ens/ENSRegistry.sol");
+var DummyDNSSEC = artifacts.require("./DummyDNSSEC.sol");
 var DNSRegistrar = artifacts.require("./DNSRegistrar.sol");
 
 var namehash = require('eth-ens-namehash');
 var sha3 = require('js-sha3').keccak_256
 var dns = require('../lib/dns.js');
+var tld = 'xyz'
 
 module.exports = function(deployer, network, accounts) {
   if(network == "ropsten") {
@@ -12,7 +14,19 @@ module.exports = function(deployer, network, accounts) {
       DNSRegistrar,
       "0xd7296B6044ffD0565062345c2EaA4017024B2D22",
       "0x112234455c3a32fd11230c42e7bccd4a84e02010",
-      dns.hexEncodeName("xyz."),
-      namehash.hash("xyz"));
+      dns.hexEncodeName(tld + "."),
+      namehash.hash(tld));
+  }else{
+    deployer.deploy([[ENSRegistry], [DummyDNSSEC]]).then(function() {
+      return ENSRegistry.deployed().then(function(ens) {
+        return DummyDNSSEC.deployed().then(function(dnssec) {
+          return deployer.deploy(DNSRegistrar, dnssec.address, ens.address, dns.hexEncodeName(tld + "."), namehash.hash(tld)).then(function() {
+            return DNSRegistrar.deployed().then(function(registrar) {
+              return ens.setSubnodeOwner(0, "0x" + sha3(tld), registrar.address);
+            });
+          });
+        });
+      });
+    });
   }
 };
