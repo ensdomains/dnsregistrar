28c69488e327dd1af885a81dbe88769698d2ddd9
decanus <dean.eigenmann@icloud.com>
2018-08-31 12:20:12 +0200

    upgraded migrations

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -8,24 +8,27 @@ var dns = require('../lib/dns.js');
 var tld = 'xyz'
 
 module.exports = function(deployer, network, accounts) {
-  if(network == "ropsten") {
-    deployer.deploy(
-      DNSRegistrar,
-      "0xd7296B6044ffD0565062345c2EaA4017024B2D22",
-      "0x112234455c3a32fd11230c42e7bccd4a84e02010",
-      dns.hexEncodeName(tld + "."),
-      namehash.hash(tld));
-  }else{
-    deployer.deploy([[ENSRegistry], [DummyDNSSEC]]).then(function() {
-      return ENSRegistry.deployed().then(function(ens) {
-        return DummyDNSSEC.deployed().then(function(dnssec) {
-          return deployer.deploy(DNSRegistrar, dnssec.address, ens.address).then(function() {
-            return DNSRegistrar.deployed().then(function(registrar) {
-              return ens.setSubnodeOwner(0, "0x" + sha3(tld), registrar.address);
-            });
-          });
-        });
-      });
+    return deployer.then(async () => {
+
+        if (network == "ropsten") {
+            await deployer.deploy(
+                DNSRegistrar,
+                "0xd7296B6044ffD0565062345c2EaA4017024B2D22",
+                "0x112234455c3a32fd11230c42e7bccd4a84e02010",
+                dns.hexEncodeName(tld + "."),
+                namehash.hash(tld)
+            );
+            return;
+        }
+
+        await deployer.deploy([[ENSRegistry], [DummyDNSSEC]]);
+
+        const ens = await ENSRegistry.deployed();
+        const dnssec = await DummyDNSSEC.deployed();
+
+        await deployer.deploy(DNSRegistrar, dnssec.address, ens.address);
+        const registrar = await DNSRegistrar.deployed();
+
+        await ens.setSubnodeOwner(0, "0x" + sha3(tld), registrar.address);
     });
-  }
 };
