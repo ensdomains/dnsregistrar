62c5f436ce573d3371c62fc0524b4aad1cd4d5e0
Jack Bates <jack@nottheoilrig.com>
2019-07-18 10:36:40 -0700

    Run the Prettier code formatter
    > prettier --write $(git ls-files)

--- test/Helpers/Utils.js
+++ test/Helpers/Utils.js
@@ -1,25 +1,29 @@
 const packet = require('dns-packet');
 
 function isException(error) {
-    let strError = error.toString();
-    return strError.includes('invalid opcode') || strError.includes('invalid JUMP') || strError.includes('revert');
+  let strError = error.toString();
+  return (
+    strError.includes('invalid opcode') ||
+    strError.includes('invalid JUMP') ||
+    strError.includes('revert')
+  );
 }
 
 function ensureException(error) {
-    assert(isException(error), error.toString());
+  assert(isException(error), error.toString());
 }
 
-function hexEncodeName(name){
-    return '0x' + packet.name.encode(name).toString('hex');
+function hexEncodeName(name) {
+  return '0x' + packet.name.encode(name).toString('hex');
 }
 
-function hexEncodeTXT(keys){
-    return '0x' + packet.answer.encode(keys).toString('hex');
+function hexEncodeTXT(keys) {
+  return '0x' + packet.answer.encode(keys).toString('hex');
 }
 
 module.exports = {
-    zeroAddress: '0x0000000000000000000000000000000000000000',
-    ensureException: ensureException,
-    hexEncodeTXT: hexEncodeTXT,
-    hexEncodeName: hexEncodeName
+  zeroAddress: '0x0000000000000000000000000000000000000000',
+  ensureException: ensureException,
+  hexEncodeTXT: hexEncodeTXT,
+  hexEncodeName: hexEncodeName
 };
