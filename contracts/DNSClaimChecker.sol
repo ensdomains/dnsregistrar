2012539b221e33ba058277e8ff1cb535580f69f9
Jack Bates <jack@nottheoilrig.com>
2019-07-18 10:39:43 -0700

    Reject expired proofs

--- contracts/DNSClaimChecker.sol
+++ contracts/DNSClaimChecker.sol
@@ -34,16 +34,14 @@ library DNSClaimChecker {
         buf.append("\x04_ens");
         buf.append(name);
         bytes20 hash;
-        uint64 inserted;
+        uint32 expiration;
         // Check the provided TXT record has been validated by the oracle
-        (, inserted, hash) = oracle.rrdata(TYPE_TXT, buf.buf);
+        (hash, expiration,) = oracle.rrdata(TYPE_TXT, buf.buf);
         if (hash == bytes20(0) && proof.length == 0) return (address(0x0), false);
 
-        require(hash == bytes20(keccak256(proof)));
+        require(hash == bytes20(keccak256(proof)) && int32(expiration - now) >= 0);
 
         for (RRUtils.RRIterator memory iter = proof.iterateRRs(0); !iter.done(); iter.next()) {
-            require(inserted + iter.ttl >= now, "DNS record is stale; refresh or delete it before proceeding.");
-
             bool found;
             address addr;
             (addr, found) = parseRR(proof, iter.rdataOffset);
--- test/mocks/DummyDNSSEC.sol
+++ test/mocks/DummyDNSSEC.sol
@@ -3,23 +3,23 @@ pragma solidity ^0.5.0;
 contract DummyDNSSEC {
     uint16 expectedType;
     bytes expectedName;
-    uint32 inception;
-    uint64 inserted;
     bytes20 hash;
+    uint32 expiration;
+    uint32 inception;
 
-    function setData(uint16 _expectedType, bytes memory _expectedName, uint32 _inception, uint64 _inserted, bytes memory _proof) public {
+    function setData(uint16 _expectedType, bytes memory _expectedName, bytes memory _proof, uint32 _expiration, uint32 _inception) public {
         expectedType = _expectedType;
         expectedName = _expectedName;
-        inception = _inception;
-        inserted = _inserted;
         if(_proof.length != 0) {
             hash = bytes20(keccak256(_proof));
         }
+        expiration = _expiration;
+        inception = _inception;
     }
 
-    function rrdata(uint16 dnstype, bytes memory name) public view returns (uint32, uint64, bytes20) {
+    function rrdata(uint16 dnstype, bytes memory name) public view returns (bytes20, uint32, uint32) {
         require(dnstype == expectedType);
         require(keccak256(name) == keccak256(expectedName));
-        return (inception, inserted, hash);
+        return (hash, expiration, inception);
     }
 }
