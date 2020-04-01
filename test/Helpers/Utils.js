7229f83b6ce5efcef717746cfe4655fe5446af00
decanus <7621705+decanus@users.noreply.github.com>
2020-04-01 15:40:13 +0200

    using test-utils

--- test/Helpers/Utils.js
+++ test/Helpers/Utils.js
@@ -1,18 +1,5 @@
 const packet = require('dns-packet');
 
-function isException(error) {
-  let strError = error.toString();
-  return (
-    strError.includes('invalid opcode') ||
-    strError.includes('invalid JUMP') ||
-    strError.includes('revert')
-  );
-}
-
-function ensureException(error) {
-  assert(isException(error), error.toString());
-}
-
 function hexEncodeName(name) {
   return '0x' + packet.name.encode(name).toString('hex');
 }
@@ -23,7 +10,6 @@ function hexEncodeTXT(keys) {
 
 module.exports = {
   zeroAddress: '0x0000000000000000000000000000000000000000',
-  ensureException: ensureException,
   hexEncodeTXT: hexEncodeTXT,
   hexEncodeName: hexEncodeName
 };
