c3fde649767e435b248c0c9921ce0e4f8870cc4b
Nick Johnson <arachnid@notdot.net>
2021-01-25 14:46:43 +1300

    Fix tests by making deployment optional:

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -4,6 +4,7 @@ require('dotenv').config();
 
 module.exports = function(deployer, network) {
     return deployer.then(async () => {
+        if(network == 'test') return;
         await deployer.deploy(TLDPublicSuffixList);
         let psl = await TLDPublicSuffixList.deployed();
         await deployer.deploy(DNSRegistrar, process.env.DNSSEC_ORACLE_ADDRESS, psl.address, process.env.ENS_ADDRESS);
